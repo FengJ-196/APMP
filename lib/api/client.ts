b7cb65eaf3c4d24ef62fda106655ecc5b9c70104
thanh <thanhphongwf@gmail.com>
2026-06-03 23:42:55 +0700

    fix: add dependencies

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -18,6 +18,7 @@ export async function fetchClient<T>(
 ): Promise<T> {
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
     ...options.headers,
   };
 
--- lib/api/index.ts
+++ lib/api/index.ts
@@ -2,3 +2,5 @@ export * from './client';
 export * from './user';
 export * from './projects';
 export * from './files';
+export * from './sourceOfTruth';
+export * from './conflicts';
